c52807c59da01e07f3988149bf4f82cbc72fd014
Matt Wiens <mwiens91@gmail.com>
2018-04-27 16:30:18 -0700

    Add scaffolding for main function

---
+++ rpi_led_lightshow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run the rpi-led-lightshow program."""
+
+from rpi_lightshow import main
+
+# Run it
+main()
---
+++ rpi_lightshow/main.py
@@ -0,0 +1,12 @@
+"""Main function for the light show."""
+
+import pyaudio
+import RPi.GPIO as GPIO
+from rpi_lightshow.constants import GPIO_PINS
+
+def pyaudio_callback(raw_audio_string, *_):
+    """Callback function for PyAudio stream."""
+    pass
+
+def main():
+    pass
