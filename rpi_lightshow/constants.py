a454a41e0eee2c278675f208aafbc2dafd431e6b
Matt Wiens <mwiens91@gmail.com>
2018-04-27 14:24:16 -0700

    Add frequency ranges of interest

--- rpi_lightshow/constants.py
+++ rpi_lightshow/constants.py
@@ -15,3 +15,14 @@ FRAMES_PER_BUFFER = 1024    # number of audio frames per buffer
 FORMAT = pyaudio.paInt16    # sampling size and format
 CHANNELS = 1                # number of audio channels
 RATE = 44100                # sampling rate in Hz
+
+# This is a list of frequency ranges of interest, based
+# mostly on intuition and testing, and also information online. Tweaking
+# these may lead to better results.
+FREQUENCY_BINS = [(60, 150),    # bass (consider going further than 150)
+                  (250, 500),   # low midrange
+                  (500, 2e3),   # midrange
+                  (2e3, 4e3),   # upper midrange
+                  (4e3, 6e3),   # "presence"
+                  (6e3, 20e3),  # "brilliance"
+                 ]
