a7cebca4b3a4bb851f1d4fa5ad76a8ed3bcc0f93
Matt Wiens <mwiens91@gmail.com>
2018-04-27 23:36:42 -0700

    Ensure 1 channel is always used

--- rpi_lightshow/constants.py
+++ rpi_lightshow/constants.py
@@ -8,8 +8,14 @@ import pyaudio
 # any of these)
 FRAMES_PER_BUFFER = 1024    # number of audio frames per buffer
 FORMAT = pyaudio.paInt16    # sampling size and format
+
+# Do NOT change this. Dealing with 2 channels is non-trivial. See
+# https://stackoverflow.com/questions/22636499/convert-multi-channel-pyaudio-into-numpy-array
+# to understand why.
 CHANNELS = 1                # number of audio channels
+
 RATE = 44100                # sampling rate in Hz
+
 
 # This is a list of frequency ranges of interest, based mostly on
 # intuition and testing, and also information online. Tweaking these may
@@ -22,6 +28,7 @@ FREQUENCY_BINS = ((60, 150),    # bass (consider going further than 150)
                   (4e3, 6e3),   # "presence"
                   (6e3, 20e3),  # "brilliance"
                  )
+
 
 # Raspberry Pi GPIO pins to use. The order here matters, and for now
 # there must be exactly 6 pins. More on this later.
